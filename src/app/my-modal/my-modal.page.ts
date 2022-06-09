464759abf518f74e34695f00a852224d21d1fba9
alanv82 <alan.moscoso.p@gmail.com>
2022-06-08 21:46:23 -0400

    se corrige el popup

--- src/app/my-modal/my-modal.page.ts
+++ src/app/my-modal/my-modal.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ModalController } from '@ionic/angular';
 
 @Component({
   selector: 'app-my-modal',
@@ -7,9 +8,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MyModalPage implements OnInit {
 
-  constructor() { }
+  constructor(private modalCtrl: ModalController) { }
 
   ngOnInit() {
+  }
+
+  dismiss(){
+    this.modalCtrl.dismiss();
   }
 
 }
