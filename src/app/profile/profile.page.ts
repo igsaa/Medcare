c9546de426a8060d6f02e92d90628470dc94a133
Ignacio Saavedra <saavedragaray.ignacio@gmail.com>
2022-07-13 02:23:49 -0400

    odificacion modulo tomar hora y perfil paciente

--- src/app/profile/profile.page.ts
+++ src/app/profile/profile.page.ts
@@ -7,7 +7,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProfilePage{
 
+//Datos ingresados manualmente para testeo - BORRAR ESTA LINEA
 nombredoctor = 'Sigmun Froid';
+centroMed = 'Centro Médico MedCare San Joaquín';
+peso = '75';
+altura = '180';
+edad= '54';
+rh= 'AB';
+enfermedad= 'Asma';
+tratamiento= 'Aplicación de penedol';
+dosis='Cada 8 horas';
 
   constructor() { }
 
